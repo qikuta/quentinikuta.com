89f4a176e3c6c5083d95c769ea91164a09188ae4
Quentin Ikuta <49662235+qikuta@users.noreply.github.com>
2023-02-07 12:13:22 -0600

    adding potree html folder for point cloud viz

---
+++ laspublish.6212/pointclouds/portal/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=15 +datum=WGS84 +units=m +no_defs",
+    "points": 69825415,
+    "boundingBox": {
+        "lx": 592499.62,
+        "ly": 5310930.99,
+        "lz": 406.45,
+        "ux": 593064.5700000002,
+        "uy": 5311495.94,
+        "uz": 971.4000000001863
+    },
+    "tightBoundingBox": {
+        "lx": 592499.62,
+        "ly": 5310930.99,
+        "lz": 406.45,
+        "ux": 593023.6900000001,
+        "uy": 5311495.94,
+        "uz": 454.21000000000006
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 4.892610549926758,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
